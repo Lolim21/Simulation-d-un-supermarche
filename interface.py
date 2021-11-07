13c284d8f53b60baef151b847b52388bef370cd5
Lolim21 <khadijazekri2011@gmail.com>
2021-11-07 12:04:10 +0100

    simulation

---
+++ interface.py
@@ -0,0 +1,66 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import Button, Label, StringVar, ttk,messagebox
+from prettytable import PrettyTable
+root=Tk()
+root.geometry('400x400')
+root.configure(bg='#008080')
+def AfficherTable():
+    msg = value_radio.get()
+    x = PrettyTable()
+    if msg == '2':
+        x.field_names = ["jours", "NCE", "NCP", "TFS",'TSmoy','TATmoy','TauC1','TauC2','Ratio1','Ratio2']
+        TT = 9 
+        import Simula2 as supe
+    else:
+        x.field_names = ["jours", "NCE", "NCP", "TFS",'TSmoy','TATmoy','TauC1','TauC2','TauC3','Ratio1','Ratio2']
+        TT = 10 
+        import yassine as  supe
+    IX=e1.get()
+    IY=e2.get()
+    IZ=e3.get()
+    if int(IX)> 29800 or int(IY)> 29800 or int(IZ)> 29800 or int(IX)<= 0 or int(IY)<= 0 or int(IZ)<= 0:
+        messagebox.showinfo("Erreur", "Germe n'est pas dans l'intervalle ")
+    else:
+        K=supe.main(IX,IY,IZ,0)
+        for j in range(41):
+            for z in range(TT):
+                    K[j][z+1] = round(K[j][z+1],4)
+            x.add_row(K[j])  
+        print(x) 
+
+
+value_radio=StringVar()
+radio_male=Radiobutton(root,text="2 caisses",value="2",bg="#29FFFF",variable=value_radio,indicatoron=0,width=10).pack(padx=15,pady=5,anchor=W)
+radio_female=Radiobutton(root,text="3 caisses",value="3",bg="#29FFFF",variable=value_radio,indicatoron=0,width=10).pack(padx=15,pady=5,anchor=W)
+tk.Label(root,text='Entrez IX',bg='#008080').pack(padx=15,pady=5,anchor=W)
+e1=Entry(root,width=200)
+
+e2=Entry(root,width=200)
+
+e3=Entry(root,width=200)
+e1.pack(padx=15,pady=5,anchor=W)
+tk.Label(root,text='Entrez IY',bg='#008080').pack(padx=15,pady=5,anchor=W)
+e2.pack(padx=15,pady=5,anchor=W)
+tk.Label(root,text='Entrez IZ',bg='#008080').pack(padx=15,pady=5,anchor=W)
+e3.pack(padx=15,pady=5,anchor=W)
+Button(root, text="Afficher les reultats", command=AfficherTable).pack(padx=15,pady=5,anchor=W)
+
+def AfficherPlot():
+    msg = value_radio.get()
+    if msg == '2':
+        import Simula2 as supe
+    else:
+        import yassine as  supe
+    IX=e1.get()
+    IY=e2.get()
+    IZ=e3.get()
+    if int(IX)> 29800 or int(IY)> 29800 or int(IZ)> 29800 or int(IX)<= 0 or int(IY)<= 0 or int(IZ)<= 0:
+        messagebox.showinfo("Erreur", "Germe n'est pas dans l'intervalle ")
+    else:
+        K=supe.main(IX,IY,IZ,1)
+
+Button(root, text="Plot", command=AfficherPlot).pack(padx=50,pady=5,anchor=W)  
+Button(root,text='Quitter',command=root.quit).pack(padx=46,pady=5,anchor=W)
+
+root.mainloop()
